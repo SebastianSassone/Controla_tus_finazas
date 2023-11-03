eb890ffb3e78fb9095cc2e31462c715d8bb56503
Sebastian Sassone <108636772+SebastianSassone@users.noreply.github.com>
2023-11-02 21:24:04 -0300

    Update filtros.js

--- Front/Controla tus finanzas/js/valances/filtros.js
+++ Front/Controla tus finanzas/js/valances/filtros.js
@@ -1,3 +1,118 @@
+const tbody = document.querySelector("#category-list tbody");
+let isGrayRow = false;
+
+const meses = {
+  enero: 1,
+  febrero: 2,
+  marzo: 3,
+  abril: 4,
+  mayo: 5,
+  junio: 6,
+  julio: 7,
+  agosto: 8,
+  septiembre: 9,
+  octubre: 10,
+  noviembre: 11,
+  diciembre: 12
+};
+
+function filtrarMes(data, mesSeleccionado) {
+  const mesNumero = meses[mesSeleccionado];
+
+  if (!mesNumero) {
+    console.error('Mes no válido');
+    return;
+  }
+
+  const resultadosFiltrados = data.filter(entry => {
+    const fechaParts = entry.fecha.split('/');
+    if (fechaParts.length === 3) {
+      const mes = parseInt(fechaParts[1], 10);
+      return mes === mesNumero;
+    }
+    return false;
+  });
+
+  // Borra las filas actuales antes de mostrar las nuevas.
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+
+  resultadosFiltrados.forEach((entry) => {
+    let row = document.createElement("tr");
+    row.dataset.id = entry.id;
+    row.innerHTML =
+      "<td>" + entry.producto + "</td>" +
+      "<td>" + entry.categoria + "</td>" +
+      "<td>" + entry.subcategoria + "</td>" +
+      "<td>" + entry.valor + "</td>" +
+      "<td>" + entry.fecha + "</td>" +
+      "<td>" + entry.hora + "</td>" +
+      "<td class='actions'>" +
+      "<div class='edit-btn' onclick='editarDetalle(this)'><div class='popup-text'>Editar</div></div>" +
+      "<div class='delete-btn' onclick='eliminarDetalle(this)'><div class 'popup-text'>Borrar</div></div>" +
+      "<div class='save-btn' onclick='guardarEdicion(this)'><div class='popup-text'>Guardar</div></div>" +
+      "</td>";
+
+    let saveBtn = row.querySelector(".save-btn");
+    saveBtn.style.display = "none";
+    if (isGrayRow) {
+      row.classList.add("gray-row");
+    }
+
+    tbody.appendChild(row);
+
+    isGrayRow = !isGrayRow;
+  });
+}
+
+async function agregarDetalle() {
+  try {
+    const response = await fetch('http://localhost:4000/valances');
+    if (!response.ok) {
+      throw new Error('Error al obtener los datos.');
+    }
+
+    const data = await response.json();
+
+    const select = document.getElementById('categoria');
+    const mesSeleccionado = select.value;
+
+    filtrarMes(data, mesSeleccionado);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+// Declaraciones de las variables que faltan.
+const met_ahorr = document.getElementById('met_ahorr');
+const tot_gas = document.getElementById('tot_gas');
+const tot_ahorr = document.getElementById('tot_ahorr');
+
+async function sumar_total_gastos() {
+  try {
+    const response = await fetch('http://localhost:4000/traer_valor_meta');
+    const data = await response.json();
+
+    let total_gastos = data;
+    met_ahorr.innerHTML = data.meta_ahorro;
+    // Puedes completar el código para manejar el total de gastos según tus necesidades.
+  } catch (error) {
+    console.error('Error al cargar los valores: ', error);
+  }
+}
+
+// Agregar un event listener para el cambio en el select.
+const select = document.getElementById('categoria');
+select.addEventListener('change', agregarDetalle);
+
+// Cargar la página con el evento 'load'.
+window.addEventListener('load', () => {
+  agregarDetalle();
+  sumar_total_gastos();
+});
+
+
 /*
 en filtros un array para cada mes se le hace push de los datos segunla fecha, se deve multiplicar asi mismo para 
 cargar la informacion o que simplemente mejor muestre en base a la haray de los eses en pantalla para ahoarra memoria
@@ -9,7 +124,7 @@ el listado global debe ser en una panatalla nueva por que tambien de debe agrega
   
   aqui solo se mostararan los datos ya no se podra editar ni eliminar ningun detalle
   
-  para agregar nuevo detalle se pregunta to local date time string lese filtro ira en detalle*/
+  para agregar nuevo detalle se pregunta to local date time string lese filtro ira en detalle
 
 let tbody = document.querySelector("#category-list tbody");
 let isGrayRow = false;
@@ -88,11 +203,11 @@ async function sumar_total_gastos(){
        let met_ahorr = document.getElementById('met_ahorr').innerHtml = data.meta_ahorro;
         /*completar*/
       
-       }catch (error) {
+      /* }catch (error) {
        console.error('Error al cargar los valores: ', error);
     };
 };
 
 window.addEventListener("load", () => {
   agregarDetalle();
-});
+});*/                
