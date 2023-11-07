ac3b93487718700dd043e6a6b5da6d37a9cf8ac5
Sebastian Sassone <108636772+SebastianSassone@users.noreply.github.com>
2023-11-06 21:31:53 -0300

    Update mostrar_clave.js

--- Front/Controla tus finanzas/js/funcionalidades/mostrar_clave.js
+++ Front/Controla tus finanzas/js/funcionalidades/mostrar_clave.js
@@ -11,3 +11,17 @@ viewPassword.addEventListener('click', (e)=>{
     click = false
   }
 })
+
+let password = document.querySelector('.login');
+let viewPassword = document.getElementById('viewPassword');
+let click = false;
+
+viewPassword.addEventListener('click', (e)=>{
+  if(!click){
+    password.type = 'text'
+    click = true
+  }else if(click){
+    password.type = 'password'
+    click = false
+  }
+})
