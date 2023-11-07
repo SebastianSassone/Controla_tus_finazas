23905f073c1ba03931d648f81214c233bbb3b885
Sebastian Sassone <108636772+SebastianSassone@users.noreply.github.com>
2023-11-07 20:15:07 -0300

    Update registro.js

--- Front/Controla tus finanzas/js/cuenta/registro.js
+++ Front/Controla tus finanzas/js/cuenta/registro.js
@@ -42,3 +42,34 @@ form_registro.addEventListener('submit', async (event) => {
      console.log(confirmPassword);
      } catch (error) {}
    });
+
+//Mostrar contraseña
+
+let password = document.getElementById('password');
+let viewPasswordReg1 = document.getElementById('viewPasswordReg1');
+let click = false;
+
+viewPasswordReg1.addEventListener('click', (e)=>{
+  if(!click){
+    password.type = 'text'
+    click = true
+  }else if(click){
+    password.type = 'password'
+    click = false
+  }
+})
+
+let confirmPassword = document.getElementById('confirmPassword');
+let viewPasswordReg1 = document.getElementById('viewPasswordReg2');
+let click = false;
+
+viewPasswordReg2.addEventListener('click', (e)=>{
+  if(!click){
+    confirmPassword.type = 'text'
+    click = true
+  }else if(click){
+    password.type = 'password'
+    click = false
+  }
+})
+
