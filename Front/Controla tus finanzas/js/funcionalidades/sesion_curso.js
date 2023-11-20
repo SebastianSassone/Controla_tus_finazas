e43c6229ed4f7f2d1c1acf3822ed1e390348da08
Sebastian Sassone <108636772+SebastianSassone@users.noreply.github.com>
2023-11-20 18:11:27 -0300

    Update routes

--- Front/Controla tus finanzas/js/funcionalidades/sesion_curso.js
+++ Front/Controla tus finanzas/js/funcionalidades/sesion_curso.js
@@ -7,3 +7,5 @@ if(registro_realizado != 0 || sesion_en_curso != 0 ){
     REGISTRARSE.style.display = 'none';
     INICIAR_SESION.style.display = 'none';
 }
+
+//Agregar lo de cuenta que no pueda accdecer con secion ni registro sin hacer o largar alerta de registrace personalizada que te lleve a registro
